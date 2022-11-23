cf37d948e16047468e6500147668aefeb952f3c8
edurevsky <edurevsky@gmail.com>
2022-11-23 16:33:09 -0300

    add ForEach module

---
+++ for_each.exs
@@ -0,0 +1,13 @@
+defmodule ForEach do
+  def inicia(colecao, funcao) do
+    executa(colecao, funcao, [])
+  end
+
+  defp executa([], _, acumulador), do: {:ok, acumulador}
+
+  defp executa([primeiro | resto] = colecao, funcao, acumulador) when is_list(colecao) do
+    executa(resto, funcao, [funcao.(primeiro) | acumulador])
+  end
+
+  defp executa(colecao, _, _), do: {:error, colecao}
+end
